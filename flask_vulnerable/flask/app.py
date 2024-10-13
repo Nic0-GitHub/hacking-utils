cbdb003c6cee9fd11ccf162027b4d31a82dcc745
Nicolas Pieroni <pieroni.nico@gmail.com>
2024-10-13 01:16:44 -0300

    Agrego info_request como endpoint para obtener e imprimir los datos de la request tanto en log como en vista

--- flask_vulnerable/flask/app.py
+++ flask_vulnerable/flask/app.py
@@ -1,8 +1,6 @@
-from flask import Flask, abort, jsonify, redirect, render_template, request, Response, session
+from flask import Flask, abort, jsonify, render_template, request, Response, session
 from flask.logging import default_handler
 import random
-
-from flask.sessions import SessionMixin
 from constants import *
 from sys import argv
 from os import path
@@ -34,41 +32,12 @@ app.logger.removeHandler(default_handler)
 werkzeug_logger = logging.getLogger('werkzeug')
 werkzeug_logger.disabled = True
 
-def cargar_sesion(session: SessionMixin, usuario):
-    session['nombre'] = usuario['nombre']
-    session['valid'] = True
-
-def limpiar_sesion(session: SessionMixin):
-    session.clear()    
-
-def add_cors_headers(response):
-    return response
-    
-    response.headers.add('Access-Control-Allow-Origin', '*')  # Permitir todos los orígenes o especifica uno
-    response.headers.add('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    return response
-
-@app.after_request
-def after_request(response):
-    return add_cors_headers(response)
-
 @app.route('/')
 def index():
-    return redirect('/iniciar_sesion')
-
-@app.route('/status')
-def status():
-    """
-        Retorna un 200 para checkear que el server esta encendido
-    """
     return Response('<h1>Server Prendido</h1>', HTTPStatus.OK)
 
 @app.route('/mensaje', methods=['POST'])
 def mensaje():
-    """
-        Permite recibir mensajes para imprimirlos en los logs
-    """
     if not request.is_json:
         abort(HTTPStatus.BAD_REQUEST)
     
@@ -86,15 +55,13 @@ def mensaje():
 
 @app.route('/calcular', methods=['POST'])
 def calcular():
-    """
-        Recibe un json con varios parametros, compuestos por:
-            key: operacion_realizar
-        y retorna el resultado de todas las keys
+    """Recibe un json con varios parametros, compuestos por:
+        key: operacion_realizar
+    y retorna el resultado de todas las keys
     """
     if not request.is_json:
         abort(HTTPStatus.BAD_REQUEST)
-    
-    app.logger.info(f"se pidio realizar calcular: {request.json}")
+        
     try:
         calculos_realizar:dict = request.json
         context = {}
@@ -110,19 +77,23 @@ def calcular():
         app.logger.error(error_res)
         return Response(error_res, HTTPStatus.BAD_REQUEST)
     
-    res = f"cuentas realizadas:\n{calculos_realizar}"
+    res = f"cuentas realizada:\n{calculos_realizar}"
     app.logger.info(res)
     
     return Response(res, HTTPStatus.OK)
 
+@app.route("/info_request", methods=['GET'])
+def info_request():
+    datos = {}
+    for key, item in request.headers.items():
+        datos[key] = item
+    datos['ip'] = request.remote_addr
+    
+    app.logger.info(f"información de la request de info_request: {request.headers};{request.remote_addr};")
+    return render_template('info_request.html', datos=datos)
+
 @app.route('/comentario', methods=['GET', 'POST'])
 def comentario():
-    """
-        GET:
-            Vista con cuestionario que permite llenar un formulario no sanitizado.
-        POST:
-            Carga una pagina con la información del formulario obtenido del get.
-    """
     match request.method:
         case 'POST':
             # Vulnerabilidad de code injection por falta de sanitización
@@ -137,38 +108,17 @@ def comentario():
         case _:
             return Response(status=HTTPStatus.METHOD_NOT_ALLOWED)
 
+
 @app.route('/usuarios/<usuario>', methods=['GET', 'POST'])
 def pagina_usuario(usuario):
-    """
-        GET:
-            Carga la pagina del usuario (Si existe, para pedir que inicie sesion)
-            En caso de estar loggeado muestra una pagina personalizada para el usuario.
-        POST:
-            Permite crear un usuario si el username esta disponible, retorna un json con nombre de usuario y pass generada para ese usuario.
-    """
     get_nombre = lambda u: u.get('nombre', '')
     match request.method:
         case 'GET':
-            pedir_iniciar_sesion_response = Response(render_template("pagina_usuario.html", usuario=usuario, mensaje="¡inicia sesión por favor!"))
-            usuario_iniciado_distinto_response = Response(render_template("pagina_usuario.html", usuario=usuario, mensaje="¡Esta NO es tu pagina de usuario!"))
+            # Vulneralibilidad de ataque de enumeración
             if usuario in map(get_nombre, usuarios):
-                # si no inicio sesión
-                if not session.get('valid'):
-                    app.logger.info(f"Se ingreso a la pagina del usuario: {usuario} (se solicito que el usuario inicie sesión)")
-                    # Vulneralibilidad de ataque de enumeración
-                    return pedir_iniciar_sesion_response
-                
-                if (nombre:=session.get('nombre')) != usuario:
-                    app.logger.info(f"Se ingreso a la pagina del usuario ({usuario}) desde un usuario logeado ({nombre})")
-                    return usuario_iniciado_distinto_response
-                
-                app.logger.info(f"El usuario {usuario} ingreso a su pagina")
-                # respuesta si tiene una sesión valida
-                return Response(render_template("pagina_usuario.html", usuario=usuario, mensaje="Bienvenido a tu página personal."))
-            
-            app.logger.info(f"Se Intento acceder a un usuario inexistente: {usuario}")
-            return Response(render_template('usuario_no_encontrado.html'),HTTPStatus.NOT_FOUND)
-        
+                app.logger.info(f"Se accedio a la pagina del usuario: {usuario}")
+                return Response(f"<h1>Hola, {usuario}</h1>")
+            return Response("<h1>usuario no encontrado</h1>",HTTPStatus.NOT_FOUND)
         case 'POST':
             import string
             if usuario in map(get_nombre, usuarios):
@@ -182,79 +132,24 @@ def pagina_usuario(usuario):
             
             app.logger.info(f"Se creo un nuevo usuario: {usuario}")
             usuarios.append(d)
-            
             return jsonify(d), HTTPStatus.CREATED
 
-@app.route('/iniciar_sesion', methods=['GET', 'POST'])
+@app.route('/iniciar_sesion', methods=['GET'])
 def iniciar_sesion():
-    """
-        GET:
-            vista para iniciar sesion
-        POST:
-            checkea valores y permite iniciar sesión
-    """
     match request.method:
         case 'GET':
-            if (session.get('valid')):
-                return redirect(f'/usuarios/{session.get('nombre')}')
             return render_template('iniciar_sesion.html')
         case 'POST':
             fields_required = ['nombre', 'password']
-            form = request.form
-            if not form or any(field not in form for field in fields_required):
+            if not request.is_json or any(field not in request.json for field in fields_required):
                 abort(HTTPStatus.BAD_REQUEST)
+            data = request.json
             for usuario in usuarios:
-                # si el usuario es valido
-                if (usuario['nombre'] == form['nombre']) and (usuario['password'] == form['password']):
-                    cargar_sesion(session, usuario)
-                    app.logger.info(f"El usuario {usuario['nombre']} inicio sesion")
-                    return redirect(f'/usuarios/{usuario['nombre']}')
-                
-            return Response("credenciales no validas", HTTPStatus.UNAUTHORIZED)
-
-@app.route('/cerrar_sesion', methods=['POST'])
-def cerrar_sesion():
-    """
-        Borra las cookies de la sesión, borrando las credenciales para el sistema.
-    """
-    if not session.get('valid'):
-        abort(HTTPStatus.BAD_REQUEST)
-    app.logger.info(f"El usuario {session['nombre']} ha cerrado su sesion")
-    limpiar_sesion(session)
-    return Response("Sesión cerrada", HTTPStatus.OK)
-
-@app.route('/restablecer_password', methods=['GET', 'POST'])
-def restablecer_password():
-      
-    # Rechazo solicitud si no esta logeado
-    if not session.get('valid'):
-        abort(HTTPStatus.UNAUTHORIZED)
-        
-    usuario_actual = session.get('nombre')
-    match request.method:
-        case 'GET':
-            return render_template('restablecer_password.html', usuario=usuario_actual)
-        
-        case 'POST':
-            nueva_password = request.form.get('nueva_password')
-            if not nueva_password or len(nueva_password) < 8:
-                app.logger.info(f"Intento fallido de restablecer contraseña para {usuario_actual}: Contraseña no válida.")
-                return Response("Contraseña no válida. Debe tener al menos 8 caracteres.", HTTPStatus.BAD_REQUEST)
+                if (usuario['nombre'] == data['nombre']) and (usuario['password'] == data['password']):
+                    session['valid'] = True
+                    return Response("Sesión iniciada", HTTPStatus.OK)
+            abort(HTTPStatus.OK)
             
-            # Buscar el usuario en la lista y actualizar la contraseña
-            for usuario in usuarios:
-                if usuario['nombre'] == usuario_actual:
-                    usuario['password'] = nueva_password
-                    app.logger.info(f"Contraseña de {usuario_actual} restablecida con éxito.")
-                    limpiar_sesion(session)
-                    return Response(f"Contraseña restablecida exitosamente para {usuario_actual}", HTTPStatus.OK)
-            
-            # Si no encuentra el usuario, se retorna un error
-            app.logger.error(f"Error inesperado: Usuario {usuario_actual} no encontrado.")
-            return Response("Error inesperado.", HTTPStatus.INTERNAL_SERVER_ERROR)
-
-    return render_template('restablecer_password.html', usuario=usuario_actual)
-
 def args_parse():
     parser = argparse.ArgumentParser(description="Process command line arguments.")
     parser.add_argument('-p', '--port', type=int, default=5000, help='Port number to run the server on')
